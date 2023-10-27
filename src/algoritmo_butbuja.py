37753ec76f1172745101415abc26165364c82ab0
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2023-10-27 21:25:41 +0200

    updated

--- src/algoritmo_butbuja.py
+++ src/algoritmo_butbuja.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     while ordenado == False: #Recorre todo hasta que no esté desordenado y por tanto no entre al if
         ordenado = True #Si no entra en el if, en el último recorrido tendra valor True y no repetirá el bucle while
         for i in range(len(SecuenciaDesordenada)-1): 
-            if SecuenciaDesordenada[i] > SecuenciaDesordenada[i+1]: #compara cada elemento con su elemento de su derecha
-                guardarNumero = SecuenciaDesordenada[i] #guarda el número de la derecha para sustituir y hacer el cambio por el de su derecha
+            if SecuenciaDesordenada[i] > SecuenciaDesordenada[i+1]: #compara cada elemento con su elemento de su izquierda
+                guardarNumero = SecuenciaDesordenada[i] #guarda el número de la derecha para sustituir y hacer el cambio por el de su izquierda que es menor
                 SecuenciaDesordenada[i] = SecuenciaDesordenada[i+1] # al menor le da el valor del mayor 
                 SecuenciaDesordenada[i+1] = guardarNumero # y al mayor le da el valor del menor 
                 ordenado = False
